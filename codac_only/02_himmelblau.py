dfb0148296e75705f18d72d28c5279b1d88f107c
godardma <mael.godard@ensta-bretagne.org>
2026-04-21 15:26:35 +0200

    2026_04_21_15_26_35

--- codac_only/02_himmelblau.py
+++ codac_only/02_himmelblau.py
@@ -6,18 +6,18 @@ f = AnalyticFunction([x], sqr(sqr(x[0])+x[1]-a)+sqr(x[0]+sqr(x[1])-b))
 
 ctc_inv = CtcInverse (f, 80)
 
-x0 = IntervalVector.constant(2,[-7,7])
+x0 = IntervalVector([[-6,7],[-6,6]])
 p = pave(x0, ctc_inv,0.01)
 
 boxes_to_contract = [IntervalVector([[-3,6.5],[-5,-2]]),
-                     IntervalVector([[-3,-1],[0,1.5]]),
-                     IntervalVector([[4,6],[0.5,2]])]
+                     IntervalVector([[-4,-2],[0,1.5]]),
+                     IntervalVector([[4,6],[0.5,2]]),
+                     IntervalVector([[-0.5,0.5],[-0.5,0.5]]),
+                     IntervalVector([[2.5,3.5],[0.5,2.5]])]
 
 fig = Figure2D("Himmelblau", GraphicOutput.VIBES | GraphicOutput.IPE)
 fig.set_window_properties([50,50],[500,500]) # set the window position and size
 fig.set_axes(x0) # set the x-axis index to 0 and its range to [-10,10], same for y with index 1
-
-# fig.draw_paving(p)
 
 L_bound = p.boxes(PavingOut.outer)
 
@@ -25,6 +25,6 @@ for box in L_bound:
   fig.draw_box(box,[Color.black(),Color.black()])
 
 for box in boxes_to_contract:
-  fig.draw_box(box,Color.blue())
+  fig.draw_box(box,StyleProperties(Color.blue(),"w:0.05"))
   box = ctc_inv.contract(box)
-  fig.draw_box(box,Color.red())
+  fig.draw_box(box,StyleProperties(Color.red(),"w:0.05"))
