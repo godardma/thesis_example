0c1245acd0163b7b1297a3cb086edcdc5969ce68
godardma <mael.godard@ensta-bretagne.org>
2026-04-27 17:46:41 +0200

    2026_04_27_17_46_41

--- codac_only/01_sivia.py
+++ codac_only/01_sivia.py
@@ -4,9 +4,9 @@ x = VectorVar(2)
 f = AnalyticFunction([x],sqrt(sqr(x[0])+sqr(x[1])))
 
 x0 = IntervalVector([[-4,4],[-4,4]])
-p = sivia(x0,f,Interval(1,3),0.1)
 
-
+epsilon = 0.1
+p = sivia(x0,f,Interval(1,3),epsilon)
 
 fig = Figure2D("SIVIA", GraphicOutput.VIBES | GraphicOutput.IPE)
 fig.set_window_properties([50,50],[500,500])
--- codac_only/04_zonotope.py
+++ codac_only/04_zonotope.py
@@ -1,21 +1,35 @@
 from codac import *
 
-z = Vector([4,1.5])
-Az = Matrix([[-0.2,0.2  ,0.08],
-             [0.1,0.04,0.18]])
+z1 = Vector([4,1.5])
+A1 = Matrix([[-0.2, 0.2, 0.08],
+             [0.1, 0.04, 0.18]])
+Z1 = Zonotope(z1,A1)
 
-fig = Figure2D("Zonotope", GraphicOutput.VIBES | GraphicOutput.IPE)
-fig.set_window_properties([50,50],[500,500]) # set the window position and size
-x0 = IntervalVector([[3,5],[1,2]])
-fig.set_axes(x0) # set the x-axis index to 0 and its range to [-10,10], same for y with index 1
+z2 = Vector([3,-0.5])
+A2 = Matrix([[0.2, -0.1],
+             [0.15,0.01]])
+Z2 = Zonotope(z2,A2)
+
+fig1 = Figure2D("Zonotope", GraphicOutput.VIBES | GraphicOutput.IPE)
+fig1.set_window_properties([50,50],[500,500]) # set the window position and size
+fig1.set_axes(IntervalVector([[3,5],[1,2]])) # set the x-axis index to 0 and its range to [-10,10], same for y with index 1
 
 
-fig.draw_zonotope(Zonotope(z,Az),StyleProperties([Color.black(),Color.gray(0.5)],"zonotope","w:0.02"))
+fig1.draw_zonotope(Z1,StyleProperties([Color.black(),Color.gray(0.5)],"zonotope","w:0.02"))
 
-for j in range (Az.cols()):
+for j in range (A1.cols()):
   if j == 0:
-    fig.draw_arrow(z,z+Az.col(j),0.1,[Color.red(),Color.red()])
+    fig1.draw_arrow(z1,z1+A1.col(j),0.1,[Color.red(),Color.red()])
   if j == 1:
-    fig.draw_arrow(z,z+Az.col(j),0.1,[Color.green(),Color.green()])
+    fig1.draw_arrow(z1,z1+A1.col(j),0.1,[Color.green(),Color.green()])
   if j == 2:
-    fig.draw_arrow(z,z+Az.col(j),0.1,[Color.blue(),Color.blue()])
+    fig1.draw_arrow(z1,z1+A1.col(j),0.1,[Color.blue(),Color.blue()])
+
+fig2 = Figure2D("Zonotope Sum", GraphicOutput.VIBES | GraphicOutput.IPE)
+fig2.set_window_properties([600,50],[500,500]) # set the window position and size
+fig2.set_axes(IntervalVector([[1,9],[-1,2.3]])) # set the x-axis index to 0 and its range to [-10,10], same for y with index 1
+
+
+fig2.draw_zonotope(Z1,StyleProperties([Color.black(),Color.gray(0.5)],"zonotope","w:0.02"))
+fig2.draw_zonotope(Z2,StyleProperties([Color.black(),Color.blue(0.5)],"zonotope","w:0.02"))
+fig2.draw_zonotope(Z1+Z2,StyleProperties([Color.black(),Color.red(0.5)],"zonotope","w:0.02"))
