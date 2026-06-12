05d6961d2fa47757079521a31181e4d1893d95b8
godardma <mael.godard@ensta-bretagne.org>
2026-06-12 19:02:50 +0200

    2026_06_12_19_02_50

--- codac_only/23_box_chains.cpp
+++ codac_only/23_box_chains.cpp
@@ -45,6 +45,36 @@ bool injectivity_criterion (const AnalyticFunction<VectorType>& f, const BoxChai
   return !lu.determinant().contains(0);
 }
 
+vector<BoxChain> BC_decomposition(const AnalyticFunction<VectorType>& f, const AnalyticFunction<VectorType>& psi, const vector<IntervalVector>& cover)
+{
+  auto L_W = cover;
+  vector<BoxChain> L_BC;
+  IntervalVector x = L_W.back();
+  L_W.pop_back();
+  while (!L_W.empty())
+  {
+    BoxChain chain;
+    chain.push_back(BoxLink(psi, x));
+    while (!L_W.empty())
+    {
+      x = L_W.back();
+      L_W.pop_back();
+      BoxLink link (psi,x);
+      if (injectivity_criterion(f,chain,link) && chain.is_neighbor(link))
+        chain.push_back(link);
+      else
+        break;
+    }
+    L_BC.push_back(chain);
+  }
+  return L_BC;
+}
+
+bool find_intersection (const vector<BoxChain>& L_BC, const AnalyticFunction<VectorType>& f)
+{
+  return true;
+}
+
 int main()
 {
   IntervalVector V1 ({{-1,1}});
@@ -58,6 +88,8 @@ int main()
   split(V1, epsilon, C1);
   split(V2, epsilon, C2);
 
+  cout << "Number of boxes in cover: " << C1.size() << endl;
+
   VectorVar X(1);
   AnalyticFunction psi_1 ({X},{cos(X[0]*PI/2),sin(X[0]*PI/2)});
   AnalyticFunction psi_2 ({X},{cos(PI+X[0]*PI/2),sin(PI+X[0]*PI/2)});
@@ -68,11 +100,43 @@ int main()
   AnalyticFunction g1 ({X},f(psi_1(X)));
   AnalyticFunction g2 ({X},f(psi_2(X)));
 
-  cout<<injectivity_criterion(f, chain1, link1)<<endl;;
+  auto L_BC1 = BC_decomposition(f, psi_1, C1);
+  auto L_BC2 = BC_decomposition(f, psi_2, C2);
 
-  for (auto& c1 : C1)
-    DefaultFigure::draw_box(g1.eval(c1));
+  auto L_BC = L_BC1;
+  for (const auto& chain : L_BC2)
+    L_BC.push_back(chain);
 
-  for (auto& c2 : C2)
-    DefaultFigure::draw_box(g2.eval(c2));
+  cout << "Number of box chains: " << L_BC.size() << endl;
+
+  ColorMap cmap_edge = ColorMap::rainbow();
+  ColorMap cmap_fill = ColorMap::rainbow(0.5);
+
+  IntervalVector axis_limit_initial ({{-1.5,1.5},{-1.5,1.5}});
+  IntervalVector axis_limit_image ({{-2.25,2.75},{-2.5,2.5}});
+
+  Figure2D fig_bc_init("box_chains_init", GraphicOutput::VIBES | GraphicOutput::IPE);
+  fig_bc_init.set_window_properties({50,50},{500,500});
+  fig_bc_init.set_axes(axis_limit_initial);
+
+
+  Figure2D fig_bc_image ("box_chains_image", GraphicOutput::VIBES | GraphicOutput::IPE);
+  fig_bc_image.set_window_properties({650,50},{500,500});
+  fig_bc_image.set_axes(axis_limit_image);
+
+  Figure2D fig_intersect ("box_chains_intersections", GraphicOutput::VIBES | GraphicOutput::IPE);
+  fig_intersect.set_window_properties({1250,50},{500,500});
+  fig_intersect.set_axes(axis_limit_image);
+
+  for (size_t i = 0; i < L_BC.size(); ++i)
+  {
+    const auto& chain = L_BC[i];
+    for (const auto& link : chain)
+    {
+      AnalyticFunction gi ({X},f(link.psi(X)));
+      double color_value = (double)i/(double)(L_BC.size()-1);
+      fig_bc_init.draw_box(link.psi.eval(link.x), {cmap_edge.color(color_value), cmap_fill.color(color_value)});
+      fig_bc_image.draw_box(gi.eval(link.x), {cmap_edge.color(color_value), cmap_fill.color(color_value)});
+    }
+  }
 }
